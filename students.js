37b25bec86b9a9c0718b05577728e5b0a07b36b2
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-08-01 14:15:42 -0400

    created a students port

---
+++ students.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+const port= 4001
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, ()=> {
+    console.log("I was needed")
+})
