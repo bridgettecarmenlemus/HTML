669ee9cb3a8d77ff86b1f23a7c2f4fd43ab0a914
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-08-01 17:03:56 -0400

    CSS

--- students.js
+++ students.js
@@ -1,11 +1,27 @@
-import express from 'express'
-import cors from 'cors'
-const app = express()
-const port= 4001
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 4001;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, ()=> {
-    console.log("I was needed")
-})
+let students = ["bob", "samantha", "lily", "grace", "jim"];
+
+app.get("/", (req, res) => {
+  let output = "<html><body><ul>"; // starts
+
+  // output = output + "hello there html"; //middle
+
+  for(let i = 0; i < students.length; i++)
+
+  output = output + "<li><b>rondolph</b></li>";
+
+  output = output + "</body></html>"; // end
+
+  res.send(output);
+});
+
+app.listen(port, () => {
+  console.log("I was needed");
+});
